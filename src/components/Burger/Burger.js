aea8a97361664a44a703351f068314c5c9124550
Anai Araya <anai.yusary@gmail.com>
2018-06-05 13:29:33 +1000

    add state to dynamically add ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,16 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
 
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)    
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        })
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+                {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     )
--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -5,14 +5,14 @@ import PropTypes from 'prop-types'
 
 class BurgerIngredient extends Component {
     render() {
-        let ingredient = null;
+        let ingredients = null;
 
         switch (this.props.type) {
             case('bread-bottom'):
-                ingredient = <div className={classes.BreadBottom}></div>
+                ingredients = <div className={classes.BreadBottom}></div>
                 break
             case('bread-top'):
-                ingredient = (
+                ingredients = (
                     <div className={classes.BreadTop}>
                         <div className={classes.Seeds1}></div>
                         <div className={classes.Seeds2}></div>
@@ -20,21 +20,21 @@ class BurgerIngredient extends Component {
                 )
                 break
             case('meat'):
-                ingredient = <div className={classes.Meat}></div>
+                ingredients = <div className={classes.Meat}></div>
                 break
             case('cheese'):
-                ingredient = <div className={classes.Cheese}></div>
+                ingredients = <div className={classes.Cheese}></div>
                 break
             case('salad'):
-                ingredient = <div className={classes.Salad}></div>
+                ingredients = <div className={classes.Salad}></div>
                 break
             case('bacon'):
-                ingredient = <div className={classes.Bacon}></div>
+                ingredients = <div className={classes.Bacon}></div>
                 break
             default:
-                ingredient = null
+                ingredients = null
         }
-        return ingredient
+        return ingredients
     }
 }
 
