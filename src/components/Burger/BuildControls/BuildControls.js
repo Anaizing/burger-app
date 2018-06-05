543a898136de86c253d62eabab0d37674f5686d1
Anai Araya <anai.yusary@gmail.com>
2018-06-05 15:43:44 +1000

    fix ingredient delete handler to not subtract past 0

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -16,6 +16,7 @@ const buildControls = (props) => (
                 key={ctrl.label} 
                 label={ctrl.label}
                 added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
             />
         ))}
     </div>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -40,6 +40,9 @@ class BurgerBuilder extends Component {
 
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type]
+        if (oldCount <= 0) {
+            return
+        }
         const updatedCount = oldCount - 1
         const updatedIngredients = {
             ...this.state.ingredients
@@ -58,7 +61,8 @@ class BurgerBuilder extends Component {
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
-                    ingredientAdded={this.addIngredientHandler} />
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler} />
             </Aux>
         )
     }
