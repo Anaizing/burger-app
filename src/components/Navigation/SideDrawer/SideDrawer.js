870c46dafb8ce818bb3293a764c50b454fc46154
Anai Araya <anai.yusary@gmail.com>
2018-06-05 22:23:29 +1000

    add responsive adjustments

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,8 +6,10 @@ import classes from './SideDrawer.css'
 const sideDrawer = (props) => {
     // ...
     return (
-        <div className={classes.SideDrawer}>
-            <Logo />
+        <div className={classes.SideDrawer} >
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
             <nav>
                 <NavigationItems />
             </nav>
