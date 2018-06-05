03ac9ac0743b6dc03f3d4d3fce339279c47fca72
Anai Araya <anai.yusary@gmail.com>
2018-06-05 13:04:50 +1000

    add burger component to container

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import classes from './BurgerIngredients.css'
 import PropTypes from 'prop-types'
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import Aux from '../../hoc/Auxiliary'
 
+import Burger from '../../components/Burger/Burger'
+
 class BurgerBuilder extends Component {
     render() {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger />
                 <div>Build Controls</div>
             </Aux>
         )
