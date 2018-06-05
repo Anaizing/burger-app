2ae0e7d9ca5c8c2aa66f3b9af399b7cff2f3e38f
Anai Araya <anai.yusary@gmail.com>
2018-06-05 22:08:53 +1000

    add sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Aux from '../../hoc/Auxiliary'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import Sidedrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => (
     <Aux>
         <Toolbar />
+        <Sidedrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
