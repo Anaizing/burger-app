5a19c8fb538f94b0353b731ffe656811dcc47aaa
Anai Araya <anai.yusary@gmail.com>
2018-06-06 00:12:58 +1000

    improve perf by adding should compUpdate

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Aux from '../../../hoc/Auxiliary'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-        return (
-        <li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-        </li>
-    )
-    })
-   
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaceCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaceContinued}>CONTINUE</Button>
-        </Aux>
-    )
-}
+class OrderSummary extends Component {
 
-export default orderSummary
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+                    </li>
+                )
+            })
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaceCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaceContinued}>CONTINUE</Button>
+            </Aux >
+        )
+    }
+}
+    
+
+
+export default OrderSummary
