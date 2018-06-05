6d8594835d17e471815565c1e025c00ce705304f
Anai Araya <anai.yusary@gmail.com>
2018-06-05 12:32:06 +1000

    add ingredients

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -20,6 +20,15 @@ const burgerIngredient = (props) => {
         case ('meat'):
             ingredient = <div className={classes.Meat}></div>
             break
+        case ('cheese'):
+            ingredient = <div className={classes.Cheese}></div>
+            break
+        case ('salad'):
+            ingredient = <div className={classes.Salad}></div>
+            break
+        case ('bacon'):
+            ingredient = <div className={classes.Bacon}></div>
+            break
     }
 }
 
