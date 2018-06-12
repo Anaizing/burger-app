1144a3dbf0f5e18df4e5f5c9dc7c0f0d110eeec3
Anai Araya <anai.yusary@gmail.com>
2018-06-12 21:45:36 +1000

    add checkkout summary styling

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import Burger from '../../Burger/Burger'
 import Button from '../../UI/Button/Button'
+import classes from './CheckoutSummary.css'
 
 const checkoutSummary = (props) => {
     return(
-        <div>
+        <div className={classes.CheckoutSummary}>
             <h1>We hope you are loving the flavour! </h1>
             <div style={{width: '300px', height: '300px', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
-            <Button btnType="Danger">CANCEL</Button>
-            <Button btnType="Success">CONTINUE</Button>
+            <Button 
+                btnType="Danger"
+                clicked>CANCEL</Button>
+            <Button 
+                btnType="Success"
+                clicked>CONTINUE</Button>
             
         </div>
     )
