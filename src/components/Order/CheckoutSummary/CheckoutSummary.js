97306f2e760ddec2086b9a678f5842ab4d4d12f9
Anai Araya <anai.yusary@gmail.com>
2018-06-12 21:38:04 +1000

    add chechout summary structure

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Burger from '../../Burger/Burger'
+import Button from '../../UI/Button/Button'
 
 const checkoutSummary = (props) => {
     return(
         <div>
-        
+            <h1>We hope you are loving the flavour! </h1>
+            <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button btnType="Danger">CANCEL</Button>
+            <Button btnType="Success">CONTINUE</Button>
+            
         </div>
     )
 }
